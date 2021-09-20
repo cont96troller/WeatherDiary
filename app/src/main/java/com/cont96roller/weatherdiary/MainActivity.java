65f7ac184fccab7b9da3a1462549b2b97754bef8
gggk105 <gggmggg105@gmail.com>
2021-09-21 01:25:38 +0900

    [BLHQ-10] 메인화면 Fragment 전환기능 완성

--- app/src/main/java/com/cont96roller/weatherdiary/MainActivity.java
+++ app/src/main/java/com/cont96roller/weatherdiary/MainActivity.java
@@ -2,29 +2,85 @@ package com.cont96roller.weatherdiary;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
+import android.util.Log;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.cont96roller.weatherdiary.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup mRadioGroup;
+    private WeatherFragment mWeatherFragment;
+    private DiaryFragment mDiaryFragment;
+    private FragmentManager mFragmentManager;
+    private FragmentTransaction mFragmentTransaction;
+
+    final private static String TAG = "pyorong";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRadioGroup = findViewById(R.id.radio_group);
+        mWeatherFragment = new WeatherFragment();
+        mDiaryFragment = new DiaryFragment();
+
+        mFragmentManager = getSupportFragmentManager();
+
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.framelayout, mWeatherFragment).commitAllowingStateLoss();
+
+
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                Log.e(TAG, "선택된값 : " + i);
+                mFragmentTransaction = mFragmentManager.beginTransaction();
+
+                if (i == R.id.radio_weather) {
+                    mFragmentTransaction.replace(R.id.framelayout, mWeatherFragment).commitAllowingStateLoss();
+
+
+                } else if (i == R.id.radio_diary) {
+                    mFragmentTransaction.replace(R.id.framelayout, mDiaryFragment).commitAllowingStateLoss();
+                }
+
+
+            }
+        });
+
+        Log.d(TAG, "onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
 
 }
