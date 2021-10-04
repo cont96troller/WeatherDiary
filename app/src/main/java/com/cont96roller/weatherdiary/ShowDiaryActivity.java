0dfe72edd034d6a5566fb57fed80258914f83093
pyorolong <38350610+pyorolong@users.noreply.github.com>
2021-10-05 02:20:40 +0900

    [BLHQ-30] 일기 조회 상세 화면 레이아웃 수정

--- app/src/main/java/com/cont96roller/weatherdiary/ShowDiaryActivity.java
+++ app/src/main/java/com/cont96roller/weatherdiary/ShowDiaryActivity.java
@@ -1,19 +1,20 @@
 package com.cont96roller.weatherdiary;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.cont96roller.weatherdiary.common.Constants;
 import com.cont96roller.weatherdiary.model.ApiInterface;
-import com.cont96roller.weatherdiary.model.DiaryModel;
 import com.cont96roller.weatherdiary.model.ResponseWeather;
 
 import retrofit2.Call;
@@ -22,10 +23,14 @@ import retrofit2.GsonConverterFactory;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class ShowDiaryActivity extends AppCompatActivity {
+public class ShowDiaryActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mButton2;
-    private Button mButton3;
+    private Button mBtnBack;
+    private ImageButton mBtnEdit;
+    private ImageButton mBtnDelete;
+    private TextView mTxtDiaryTitle;
+    private TextView mTxtDiaryContents;
+    private TextView mTextTemperature;
     private Context mContext;
 
 
@@ -35,15 +40,34 @@ public class ShowDiaryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_diary);
         mContext = this;
 
+        initView();
+        getWeatherInfo();
+
+    }
+
+    private void initView() {
+        mBtnBack = findViewById(R.id.btn_back);
+        mBtnEdit = findViewById(R.id.img_btn_edit);
+        mBtnDelete = findViewById(R.id.img_btn_delete);
+        mTxtDiaryTitle = findViewById(R.id.txt_diary_title);
+        mTxtDiaryContents = findViewById(R.id.txt_diary_contents);
+        setOnClicks();
+    }
+
+    private void setOnClicks() {
+        mBtnBack.setOnClickListener(this);
+        mBtnEdit.setOnClickListener(this);
+        mBtnDelete.setOnClickListener(this);
+    }
+
+    private void getWeatherInfo() {
         String lat = "37.65171906925866";
         String lot = "127.07728375544342";
 
-
-        Retrofit client = new Retrofit.Builder().baseUrl("http://api.openweathermap.org").addConverterFactory(GsonConverterFactory.create()).build();
-
+        Retrofit client = new Retrofit.Builder().baseUrl(Constants.WEATHER_BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
 
         ApiInterface service = client.create(ApiInterface.class);
-        Call<ResponseWeather> call = service.requestWeather("d36d81339b59c0868af503708d9057b8", Double.valueOf(lat), Double.valueOf(lot));
+        Call<ResponseWeather> call = service.requestWeather(Constants.API_APP_KEY, Double.valueOf(lat), Double.valueOf(lot));
 
         call.enqueue(new Callback<ResponseWeather>() {
             @Override
@@ -52,20 +76,15 @@ public class ShowDiaryActivity extends AppCompatActivity {
                     ResponseWeather responseWeather = response.body();
                     String main = responseWeather.getWeather().get(0).getMain();
                     String icon = responseWeather.getWeather().get(0).getIcon();
-                    TextView textView = findViewById(R.id.txt_temp);
-                    ImageView imageView = findViewById(R.id.imageView);
+                    TextView textView = findViewById(R.id.txt_weather_status);
+                    ImageView imageView = findViewById(R.id.img_weather);
                     textView.setText(main);
-                    String url = "https://openweathermap.org/img/w/" + icon + ".png";
+                    String url = Constants.PREFIX_WEATHER_ICON_URL + icon + Constants.SUFFIX_WEATHER_ICON_URL;
                     Glide.with(mContext)
                             .load(url)
                             .into(imageView);
-
-
                 }
             }
-
-
-
 
 
             @Override
@@ -73,14 +92,24 @@ public class ShowDiaryActivity extends AppCompatActivity {
 
             }
         });
+    }
 
-        mButton3 = findViewById(R.id.btn_back);
-        mButton3.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_back:
                 finish();
-            }
-        }));
+                break;
 
+            case R.id.img_btn_edit:
+                Toast.makeText(mContext, "수정 버튼 선택", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.img_btn_delete:
+                Toast.makeText(mContext, "삭제 버튼 선택", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 }
