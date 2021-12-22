7b9d41f3790b89bb77d768a9209fe008e41f5de9
G3A190505 <cont96roller@gmail.com>
2021-12-22 17:19:09 +0900

    2021-12-22(1)

--- app/src/main/java/com/cont96roller/weatherdiary/MainActivity.java
+++ app/src/main/java/com/cont96roller/weatherdiary/MainActivity.java
@@ -20,13 +20,14 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.cont96roller.weatherdiary.adapter.DiaryAdapter;
+import com.cont96roller.weatherdiary.databinding.ActivityMainBinding;
 import com.cont96roller.weatherdiary.interfaces.TestInterface;
 import com.cont96roller.weatherdiary.model.DiaryModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements TestInterface, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     //사용하지않는 변수정리
     //각종 변수 선언
     private RadioGroup mRadioGroup;
@@ -36,18 +37,18 @@ public class MainActivity extends AppCompatActivity implements TestInterface, Vi
     private DiaryFragment mDiaryFragment;
     private FragmentManager mFragmentManager;
     private FragmentTransaction mFragmentTransaction;
-    private Button mBtnMoveWrite;
-    private Button mButton2;
-    private View mView;
-    private Button mBtnBGM;
+//    private Button mBtnMoveWrite;
+//    private Button mButton2;
+//    private View mView;
+//    private Button mBtnBGM;
     private Context mContext;
-    private String mPersonName = "";
+//    private String mPersonName = "";
     List<Diary> mDiaryList = null;
-    List<Diary> diaryList;
     private DiaryDB mDiaryDB = null;
     private DiaryAdapter mDiaryAdater;
     private Button mWriteButton;
     private RecyclerView mRecyclerView;
+    private ActivityMainBinding binding;
 
     final public static String TAG = "mkkang";
 
@@ -57,10 +58,12 @@ public class MainActivity extends AppCompatActivity implements TestInterface, Vi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        setContentView(R.layout.activity_main);
 
         mContext = this;
-        mWriteButton = findViewById(R.id.btn_write);
+//        mWriteButton = findViewById(R.id.btn_write);
         mContext = getApplicationContext();
         mDiaryAdater = new DiaryAdapter(mDiaryList);
         mDiaryDB = DiaryDB.getInstance(this);
@@ -70,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements TestInterface, Vi
             @Override
             public void run() {
                 try {
-                    diaryList = DiaryDB.getInstance(mContext).diaryDao().getAll();
+                    mDiaryList = DiaryDB.getInstance(mContext).diaryDao().getAll();
                     mDiaryAdater = new DiaryAdapter(mDiaryList);
                     mDiaryAdater.notifyDataSetChanged();
                     mRecyclerView.setAdapter(mDiaryAdater);
@@ -89,30 +92,31 @@ public class MainActivity extends AppCompatActivity implements TestInterface, Vi
         //버튼 몰아서 정리
         //ViewBinding 먼저사용하기
 
-        mWriteButton.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), WriteDiaryActivity.class);
-            startActivity(intent);
-        });
+//        mWriteButton.setOnClickListener(v -> {
+//            Intent intent = new Intent(getApplicationContext(), WriteDiaryActivity.class);
+//            startActivity(intent);
+//        });
 
-        mBtnBGM = findViewById(R.id.btn_bgm);
-        mBtnBGM.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startService(new Intent(getApplicationContext(), MusicService.class));
-            }
-        });
+
+//        mBtnBGM = findViewById(R.id.btn_bgm);
+//        mBtnBGM.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startService(new Intent(getApplicationContext(), MusicService.class));
+//            }
+//        });
 
         //값이 넘어오는지 테스트하는 코드
-        DiaryModel diaryModel;
-        DiaryModel diaryModel2;
+//        DiaryModel diaryModel;
+//        DiaryModel diaryModel2;
         //한글 사용 금지
         //주석에도 한글 금지
-        diaryModel = new DiaryModel("맑음", "이것은 제목", 0L);
-        diaryModel2 = new DiaryModel("흐림", "이것은 제목2", 0L);
-        ArrayList<DiaryModel> diaryList = new ArrayList<>();
-        diaryList.add(diaryModel);
-        diaryList.add(diaryModel2);
-        diaryList.add(new DiaryModel("바람많은", "춥다", 0L));
+//        diaryModel = new DiaryModel("맑음", "이것은 제목", 0L);
+//        diaryModel2 = new DiaryModel("흐림", "이것은 제목2", 0L);
+//        ArrayList<DiaryModel> diaryList = new ArrayList<>();
+//        diaryList.add(diaryModel);
+//        diaryList.add(diaryModel2);
+//        diaryList.add(new DiaryModel("바람많은", "춥다", 0L));
 
         //findViewById 사용하여 버튼 연결
         mRadioGroup = findViewById(R.id.radio_group);
@@ -126,51 +130,55 @@ public class MainActivity extends AppCompatActivity implements TestInterface, Vi
         mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-
                 mFragmentTransaction = mFragmentManager.beginTransaction();
                 //삼항연산자 사용으로 코드 줄이기
-                if (i == R.id.radio_weather) {
-                    mFragmentTransaction.replace(R.id.framelayout, mWeatherFragment).commitAllowingStateLoss();
-                    mRadioDiary.setTypeface(null, Typeface.NORMAL);
-                    mRadioWeather.setTypeface(null, Typeface.BOLD);
-                } else if (i == R.id.radio_diary) {
-                    mFragmentTransaction.replace(R.id.framelayout, mDiaryFragment).commitAllowingStateLoss();
-                    mRadioDiary.setTypeface(null, Typeface.BOLD);
-                    mRadioWeather.setTypeface(null, Typeface.NORMAL);
-                }
+                mFragmentTransaction.replace(R.id.framelayout, mDiaryFragment).commitAllowingStateLoss();
+                mFragmentTransaction.replace(R.id.framelayout, mWeatherFragment).commitAllowingStateLoss();
+
+//                if (i == R.id.radio_weather) {
+//                    mFragmentTransaction.replace(R.id.framelayout, mWeatherFragment).commitAllowingStateLoss();
+//                    mRadioDiary.setTypeface(null, Typeface.NORMAL);
+//                    mRadioWeather.setTypeface(null, Typeface.BOLD);
+//                } else if (i == R.id.radio_diary) {
+//                    mFragmentTransaction.replace(R.id.framelayout, mDiaryFragment).commitAllowingStateLoss();
+//                    mRadioDiary.setTypeface(null, Typeface.BOLD);
+//                    mRadioWeather.setTypeface(null, Typeface.NORMAL);
+//                }
             }
         });
         mRadioWeather.setChecked(true);
-        Log.d(TAG, "onCreate");
+//        Log.d(TAG, "onCreate");
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart");
-    }
+    //oncreate 끝나는곳 밖으로 최대한 메소드 빼기
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Log.d(TAG, "onStart");
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Log.d(TAG, "onResume");
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Log.d(TAG, "onPause");
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Log.d(TAG, "onDestroy");
+//    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-    }
-
-    @Override
-    public void testMkMk() {
-    }
+//    @Override
+//    public void testMkMk() {
+//    }
 
     @Override
     public void onClick(View view) {
@@ -185,4 +193,5 @@ public class MainActivity extends AppCompatActivity implements TestInterface, Vi
                 break;
         }
     }
+
 }
